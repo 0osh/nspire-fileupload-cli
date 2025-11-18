13cb5cf13b7754d42b8d471b40c153edd74d6cd7
/oosh <132991550+0osh@users.noreply.github.com>
2025-11-19 10:55:33 +1100

    Update main.go
    Added comments and missing argument error info

--- main.go
+++ main.go
@@ -6,19 +6,27 @@ package main
 // #include "upload.h"
 import "C"
 import (
+	"fmt"
 	"os"
 	"path"
 )
 
 func main() {
+	// check if theres enough arguments
 	if len(os.Args) < 2 {
+		fmt.Println("Missing argument: Please provide the file to send as an argument")
 		return
 	}
 
+	// get args
 	fsrc := C.CString(os.Args[1])
 	fdest := C.CString(path.Base(os.Args[1]))
+	// free them when the function ends cause good practice ig
+	defer C.free(unsafe.Pointer(fsrc))
+	defer C.free(unsafe.Pointer(fdest))
 
+	// upload file (funciton declated in upload.c)
 	ret := C.upload(fsrc, fdest)
-
+	
 	os.Exit(int(ret))
 }
